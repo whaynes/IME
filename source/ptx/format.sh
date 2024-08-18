d0420f5d3fb43ed16d0d404d3490c106d7966d94
William Haynes <whaynes@maritime.edu>
2024-08-17 20:35:51 -0400

    Format all files using ptx-format

--- source/ptx/format.sh
+++ source/ptx/format.sh
@@ -2,7 +2,6 @@
 for f in *.ptx
 do
 	echo formatting $f
-	ptx-format -c format.cfg -f $f -o temp.fmt
-	mv temp.fmt $f
-	rm temp.fmt
+	ptx-format -c format.cfg -f $f -o $f.temp
+  mv $f.temp $f
 done
